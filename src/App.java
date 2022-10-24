bd362d04593aa8a63cd7faae2714992a69b379c3
SantaRenata <renatasanta2002@gmail.com>
2022-10-24 17:30:02 +0200

    Feladat megoldva

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Sánta Renáta Diána, Szoft II N, 2022-10-24");
+        NameRegistry nameRegistry = new NameRegistry();
+        ArrayList<String> nameList = nameRegistry.getNames();
+        for(String name : nameList){
+            System.out.println(name);
+        }
     }
 }
--- src/NameRegistry.java
+++ src/NameRegistry.java
@@ -1,3 +1,13 @@
-public class NameRegistry {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NameRegistry implements Name{
+
+    @Override
+    public ArrayList<String> getNames() {
+        String[] nevek = {"Béla","Géza"};
+        ArrayList<String> nameList = new ArrayList<>(Arrays.asList(nevek));
+        return nameList;
+    }
     
 }
